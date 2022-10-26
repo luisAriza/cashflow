72508634df9c6ce3877b959585e95bcb2c46afa3
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-26 10:48:26 -0500

    Formulario para agregar

--- src/components/Action.vue
+++ src/components/Action.vue
@@ -1,7 +1,35 @@
 <template>
 	<button @click="showModal = true">Agregar Movimiento</button>
 	<teleport to="#app">
-		<Modal v-show="showModal" @close="showModal = false">Aquí formulario</Modal>
+		<Modal v-show="showModal" @close="showModal = false">
+			<form @submit.prevent="submit">
+				<div class="field">
+					<label for="">Titulo</label>
+					<input type="text" v-model="title" />
+				</div>
+				<div class="field">
+					<label for="">Monto</label>
+					<input type="number" v-model="amount" />
+				</div>
+				<div class="field">
+					<label for="">Descripcion</label>
+					<textarea row="4" v-model="description"></textarea>
+				</div>
+				<div class="field">
+					<label class="radio-label">
+						<input type="radio" v-model="movementType" value="Ingreso" />
+						<span>Ingreso</span>
+					</label>
+					<label class="radio-label">
+						<input type="radio" v-model="movementType" value="Gasto" />
+						<span>Gasto</span>
+					</label>
+				</div>
+				<div class="action">
+					<button>Agregar movimiento</button>
+				</div>
+			</form>
+		</Modal>
 	</teleport>
 </template>
 
@@ -10,6 +38,14 @@
 	import Modal from "./Modal.vue";
 
 	const showModal = ref(false);
+	const title = ref("");
+	const amount = ref(0);
+	const description = ref("");
+	const movementType = ref("Ingreso");
+
+	const submit = (): void => {
+		showModal.value = false;
+	};
 </script>
 
 <style scoped lang="scss">
